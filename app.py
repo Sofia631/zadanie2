ff7cc805a88dd113c5e4743275ebd6e126b20029
София Лиховодова <163521821+PODYMAI@users.noreply.github.com>
2025-06-11 13:09:47 +0700

    Добавлены определения функций в main

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+def greet(name):
+    return f"Привет, {name}!"
+
+def add(a, b):
+    return a + b
+
+def is_even(num):
+    return num % 2 == 0
+
+def multiply(a, b):
+    return a * b
+
 def main():
     print(greet("Пользователь"))
     print("Сумма:", add(10, 20))
     print("Чётное?", is_even(4))
     print("Произведение:", multiply(3, 4))
+
+if __name__ == "__main__":
+    main()
 
